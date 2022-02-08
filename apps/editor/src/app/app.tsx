b6a2735b04ae833c8fa69392b90d328c05949053
Ricky Hsieh <chen-yuan.hsieh@houzz.com>
2022-02-08 15:02:45 +0800

    feat: setup

---
+++ apps/editor/src/app/app.tsx
@@ -0,0 +1,12 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="editor" />
+      <div />
+    </>
+  );
+}
+
+export default App;
---
+++ libs/blocks/src/lib/blocks.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface BlocksProps {}
+
+export function Blocks(props: BlocksProps) {
+  return (
+    <div>
+      <h1>Welcome to Blocks!</h1>
+    </div>
+  );
+}
+
+export default Blocks;
