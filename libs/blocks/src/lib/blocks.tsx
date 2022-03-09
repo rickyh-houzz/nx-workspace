fc83122c5d9ff1c4c65ef1e4028ee381d8d68e45
Ricky Hsieh <chen-yuan.hsieh@houzz.com>
2022-03-09 20:37:56 +0800

    set env var

--- libs/blocks/src/lib/blocks.tsx
+++ libs/blocks/src/lib/blocks.tsx
@@ -6,7 +6,7 @@ export interface BlocksProps {}
 export function Blocks(props: BlocksProps) {
   return (
     <div>
-      <h1>Welcome to Blocks!</h1>
+      <h1>Welcome to Blocks!!!</h1>
       {utils}
     </div>
   );
